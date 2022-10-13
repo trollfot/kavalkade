1561e560750718185207dec7e775fcad0a14912d
Souheil Chelfouh <trollfot@gmail.com>
2022-10-13 16:34:29 +0200

    Added metadata sugar, as example

--- src/kavalkade/controllers/models.py
+++ src/kavalkade/controllers/models.py
@@ -1,28 +1,33 @@
-from kavalkade.controllers import router
 from knappe.decorators import json, html, context
 from knappe import HTTPError, Response
+from kavalkade.controllers import router
+from kavalkade.app.models import ModelInfo
 
 
-def get_model(request):
+def get_model(request) -> ModelInfo:
     model = request.app.models.get(request.params["model"])
     if model is None:
         raise HTTPError(404)
-    if not model.metadata.get('addable', False):
+    return model
+
+
+def get_addable_model(request) -> ModelInfo:
+    model = get_model(request)
+    if not model.metadata.get("addable", False):
         raise HTTPError(403)
     return model
 
 
 @router.register("/schemas/{model}")
-@json
 @context(get_model)
-def model_schema(request, model):
+def model_schema(request, model: ModelInfo) -> Response:
     return Response.from_json(200, body=model.schema)
 
 
 @router.register("/models/{model}")
 @html("jsonschema_form")
-@context(get_model)
-def model_form(request, model):
+@context(get_addable_model)
+def model_form(request, model: ModelInfo) -> dict:
     return {
         "schema": model.schema,
         "url": f"/schemas/{request.params['model']}",
@@ -32,7 +37,7 @@ def model_form(request, model):
 
 @router.register("/models/{model}/save", methods=('POST',))
 @json
-@context(get_model)
-def model_save(request, model):
+@context(get_addable_model)
+def model_save(request, model: ModelInfo) -> dict:
     instance = model.model(**request.data.json)
-    print(repr(instance))
+    return request.data.json
