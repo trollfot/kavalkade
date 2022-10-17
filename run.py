f0bc9a23ea135012dc3d5440d931b3a47eee70cd
Souheil Chelfouh <trollfot@gmail.com>
2022-10-17 20:18:24 +0200

    Cleanup start

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from minicli import cli, run
-import logging
 
 
 def configure_logging(debug=False):
     import sys
+    import logging
 
     log_level = logging.DEBUG if debug else logging.WARNING
     stream_handler = logging.StreamHandler(stream=sys.stdout)
@@ -33,26 +33,24 @@ def create_web_app():
 
 @cli
 def http(debug: bool = False):
-
-    import sys
     import asyncio
     from aiowsgi import create_server
     from kavalkade.services.fswatcher import fswatcher
     from kavalkade.services.clock import clock
 
     configure_logging(debug)
-    app = create_web_app()
 
+    app = create_web_app()
     loop = asyncio.new_event_loop()
     app.services.bind(loop)
     app.services.add('clock', clock(app, 3))
     app.services.add('file_watcher', fswatcher(app, '/tmp'))
     app.services.add('websockets', app.websockets.serve())
-    app.services.start()
+    wsgi_server = create_server(app, loop=loop, port=8000)
 
-    srv = create_server(app, loop=loop, port=8000)
     try:
-        srv.run()
+        app.services.start()
+        wsgi_server.run()
     except:
         loop.run_until_complete(app.services.stop())
     finally:
